d789341cb2efe2005058f0e727797de01641e072
subratbhola <subratbhola07@gmail.com>
2021-12-24 23:17:38 +0530

    vue binding

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,14 @@
     <div> Hello {{name}}</div> 
     <!-- with v-text directive -->
     <div v-text="directivetext"></div> 
-    <!-- <div v-html="ntmlbind"></div> -->
+    <div v-html="ntmlbind"></div>
+    <button v-bind:id="propBinding" v-bind:disabled="propBinding2">Submit</button>
 
-
+    <!-- class binding -->
+    <h2 v-bind:class="'red'">class red</h2>
+    <h2 v-bind:class="isSoldout ? 'red':'green'">conditional class</h2>
+    <p v-bind:class="['red','italic']">array of class bind</p>
+    <p v-bind:class="{red:isSoldout}">object of class bind</p>
   </div>
 </template>
 
@@ -22,9 +27,10 @@ export default {
     return {
       name:'Subrat',
       directivetext:'directive binding',
-      ntmlbind='<b>Html Binding</b>',
-      // propBinding='id1',
-      // propBinding2=true
+      ntmlbind:'<b>Html Binding</b>',
+      propBinding:'id1',
+      propBinding2:true,
+      isSoldout:true
     }
   }
 }
@@ -38,5 +44,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.red {
+  color:red;
+}
+
+.green {
+  color: green;
+}
+
+.italic {
+  font-weight:800;
 }
 </style>
